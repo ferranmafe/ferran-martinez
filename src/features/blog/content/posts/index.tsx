6ee9c2abec1a0a815ff881c0f5df839126c54eb6
Ferran Martínez <ferran.mafe@gmail.com>
2026-02-08 11:33:14 +0100

    Fixing build

--- src/features/blog/content/posts/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/blog/posts/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/blog/posts/"!</div>
-}
